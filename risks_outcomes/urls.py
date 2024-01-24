f1de21a81a00b22f0745c4df27ea14ad269c171d
Luke Skinner <laskinner@gmail.com>
2024-01-24 02:51:08 +0100

    Added Hazards and Outcomes views, heavily edited views

--- risks_outcomes/urls.py
+++ risks_outcomes/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HazardList.as_view(), name='home'),
+    path('', views.HazardList.as_view(), name='hazards'),
+    path('outcomes', views.OutcomeList.as_view(), name='outcomes'),
+    # path('<slug:slug>/', views.hazard_detail, name='hazard_detail'),
+    # path('<slug:slug>/', views.outcome_detail, name='outcome_detail'),
 ]
--- risks_outcomes/views.py
+++ risks_outcomes/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Hazard 
+from .models import Hazard, Outcome 
 
 # Create your views here.
 
 class HazardList(generic.ListView):
     queryset = Hazard.objects.all()
-    template_name = "risks_outcomes/index.html"
+    template_name = "risks_outcomes/hazards_list.html"
+    paginate_by = 6
+
+class OutcomeList(generic.ListView):
+    queryset = Outcome.objects.all()
+    template_name = "risks_outcomes/outcomes_list.html"
+    paginate_by = 6
