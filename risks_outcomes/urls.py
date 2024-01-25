ca707fccbe848dc5d3ccd5c6d6dcc3853840150b
Luke Skinner <laskinner@gmail.com>
2024-01-25 02:20:32 +0100

    Added blog view, fixed bugs related to blog view displaying

--- risks_outcomes/urls.py
+++ risks_outcomes/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.HazardList.as_view(), name='hazards'),
-    path('outcomes', views.OutcomeList.as_view(), name='outcomes'),
-    # path('<slug:slug>/', views.hazard_detail, name='hazard_detail'),
-    # path('<slug:slug>/', views.outcome_detail, name='outcome_detail'),
+    path('outcomes/', views.OutcomeList.as_view(), name='outcomes'),
+    path('hazards/<slug:slug>/', views.hazard_detail, name='hazard_detail'),
+    path('outcomes/<slug:slug>/', views.outcome_detail, name='outcome_detail'),
 ]
--- risks_outcomes/views.py
+++ risks_outcomes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Hazard, Outcome 
 
@@ -13,3 +13,50 @@ class OutcomeList(generic.ListView):
     queryset = Outcome.objects.all()
     template_name = "risks_outcomes/outcomes_list.html"
     paginate_by = 6
+
+def hazard_detail(request, slug):
+    """
+    Display an individual :model:`risks_outcomes.Hazard`.
+
+    **Context**
+
+    ``hazard``
+        An instance of :model:`risks_outcomes.Hazard`.
+
+    **Template:**
+
+    :template:`risks_outcomes/hazard_detail.html`
+    """
+
+    queryset = Hazard.objects.all()
+    hazard = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "risks_outcomes/hazard_detail.html",
+        {"hazard": hazard},
+    )
+
+def outcome_detail(request, slug):
+    """
+    Display an individual :model:`risks_outcomes.Outcome`.
+
+    **Context**
+
+    ``outcome``
+        An instance of :model:`risks_outcomes.Outcome`.
+
+    **Template:**
+
+    :template:`risks_outcomes/outcome_detail.html`
+    """
+
+    queryset = Outcome.objects.all()
+    outcome = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "risks_outcomes/outcome_detail.html",
+        {"outcome": outcome},
+    )
+
