c37a3e538f3076aa9283e2a6a9d45a216d04e219
Luke Skinner <laskinner@gmail.com>
2024-01-25 14:17:39 +0100

    Registered comments model correctly

--- risks_outcomes/admin.py
+++ risks_outcomes/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Hazard, Outcome
+from .models import Hazard, Outcome, Comment
 
 # Register your models here.
 admin.site.register(Hazard)
 admin.site.register(Outcome)
+admin.site.register(Comment)
