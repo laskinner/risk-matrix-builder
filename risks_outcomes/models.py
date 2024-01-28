1cd7c235e6bd97aa6f1749cd3e6072c2025c4530
Luke Skinner <laskinner@gmail.com>
2024-01-28 12:22:48 +0100

    Fixed error caused from formatting

--- risks_outcomes/models.py
+++ risks_outcomes/models.py
@@ -84,5 +84,5 @@ class Document(models.Model):
                                 blank=True)
 
     def __str__(self):
-        return (f"Document for {self.hazard.name if self.hazard "
-                f"else self.outcome.name}")
+        return f
+        "Document for {self.hazard.name if self.hazard else self.outcome.name}"
