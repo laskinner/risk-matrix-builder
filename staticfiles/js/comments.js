35a5ad8daa91131a5778faa2b55bf10337ff5338
Luke Skinner <laskinner@gmail.com>
2024-04-25 01:43:08 +0200

    Added script to reset form

--- staticfiles/js/comments.js
+++ staticfiles/js/comments.js
@@ -1,3 +1,17 @@
+function resetForm() {
+  commentForm.reset(); // Reset all input fields in the form
+}
+
+// Add an event listener for form submission
+commentForm.addEventListener("submit", (event) => {
+  // Prevent the default form submission behavior
+  event.preventDefault();
+
+  // Place additional needed from validation here
+
+  resetForm(); // Resets the form fields
+});
+
 const editButtons = document.getElementsByClassName("btn-edit");
 const commentText = document.getElementById("id_body");
 const commentForm = document.getElementById("commentForm");
@@ -7,40 +21,45 @@ const deleteButtons = document.getElementsByClassName("btn-delete");
 const deleteConfirm = document.getElementById("deleteConfirm");
 
 /**
-* Initializes edit functionality for the provided edit buttons.
-* 
-* For each button in the `editButtons` collection:
-* - Retrieves the associated comment's ID upon click.
-* - Fetches the content of the corresponding comment.
-* - Populates the `commentText` input/textarea with the comment's content for editing.
-* - Updates the submit button's text to "Update".
-* - Sets the form's action attribute to the `edit_comment/{commentId}` endpoint.
-*/
+ * Initializes edit functionality for the provided edit buttons.
+ *
+ * For each button in the `editButtons` collection:
+ * - Retrieves the associated comment's ID upon click.
+ * - Fetches the content of the corresponding comment.
+ * - Populates the `commentText` input/textarea with the comment's content for editing.
+ * - Updates the submit button's text to "Update".
+ * - Sets the form's action attribute to the `edit_comment/{commentId}` endpoint.
+ */
 
 for (let button of editButtons) {
   button.addEventListener("click", (e) => {
     let commentId = e.target.getAttribute("comment_id");
     let slug = e.target.dataset.slug;
     let commentType = e.target.dataset.type;
-    let commentContent = document.getElementById(`comment${commentId}`).innerText;
+    let commentContent = document.getElementById(
+      `comment${commentId}`,
+    ).innerText;
 
     commentText.value = commentContent;
     submitButton.innerText = "Update";
     // Construct the correct URL
-    commentForm.setAttribute("action", `/${commentType}/${slug}/comment/${commentId}/edit/`);
+    commentForm.setAttribute(
+      "action",
+      `/${commentType}/${slug}/comment/${commentId}/edit/`,
+    );
   });
 }
 
 /**
-* Initializes deletion functionality for the provided delete buttons.
-* 
-* For each button in the `deleteButtons` collection:
-* - Retrieves the associated comment's ID upon click.
-* - Updates the `deleteConfirm` link's href to point to the 
-* deletion endpoint for the specific comment.
-* - Displays a confirmation modal (`deleteModal`) to prompt 
-* the user for confirmation before deletion.
-*/
+ * Initializes deletion functionality for the provided delete buttons.
+ *
+ * For each button in the `deleteButtons` collection:
+ * - Retrieves the associated comment's ID upon click.
+ * - Updates the `deleteConfirm` link's href to point to the
+ * deletion endpoint for the specific comment.
+ * - Displays a confirmation modal (`deleteModal`) to prompt
+ * the user for confirmation before deletion.
+ */
 
 for (let button of deleteButtons) {
   button.addEventListener("click", (e) => {
