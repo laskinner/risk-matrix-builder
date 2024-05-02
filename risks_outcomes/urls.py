237357539e49087b4ee1d53aa2bfd3a11a55d8e1
Luke Skinner <laskinner@gmail.com>
2024-05-02 16:44:29 +0200

    Updated URL for deleting comments in urls.py

--- risks_outcomes/urls.py
+++ risks_outcomes/urls.py
@@ -2,17 +2,23 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HazardList.as_view(), name='hazards'),
-    path('outcomes/', views.OutcomeList.as_view(), name='outcomes'),
-    path('hazards/<slug:slug>/', views.hazard_detail, name='hazard_detail'),
-    path('outcomes/<slug:slug>/', views.outcome_detail, name='outcome_detail'),
+    path("", views.HazardList.as_view(), name="hazards"),
+    path("outcomes/", views.OutcomeList.as_view(), name="outcomes"),
+    path("hazards/<slug:slug>/", views.hazard_detail, name="hazard_detail"),
+    path("outcomes/<slug:slug>/", views.outcome_detail, name="outcome_detail"),
     path(
-        '<str:comment_type>/<slug:slug>/comment/<int:comment_id>/edit/',
-        views.comment_edit, name='comment_edit'),
+        "<str:comment_type>/<slug:slug>/comment/<int:comment_id>/edit/",
+        views.comment_edit,
+        name="comment_edit",
+    ),
     path(
-        'hazards/<slug:slug>/comments/<int:comment_id>/delete/',
-        views.hazard_comment_delete, name='hazard_comment_delete'),
+        "hazards/<slug:slug>/hazard/comments/<int:comment_id>/delete/",
+        views.hazard_comment_delete,
+        name="hazard_comment_delete",
+    ),
     path(
-        'outcomes/<slug:slug>/comments/<int:comment_id>/delete/',
-        views.outcome_comment_delete, name='outcome_comment_delete'),
+        "outcomes/<slug:slug>/outcome/comments/<int:comment_id>/delete/",
+        views.outcome_comment_delete,
+        name="outcome_comment_delete",
+    ),
 ]
